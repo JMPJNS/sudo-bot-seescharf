6c2f07143b6aed31e6cc3d9ae675ded9dc52baf4
JMPJNS <jonas@jmp.blue>
2020-04-06 20:39:50 +0200

    initial 2.0

---
+++ OllekBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OllekBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
