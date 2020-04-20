af4f0b2370fe697f51b29f7343aee2ef05667b43
Jonas <jonas@jmp.blue>
2020-04-20 16:10:24 +0200

    gitignore

---
+++ OllekBot/Secrets/BotConfig.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace OllekBot
+{
+    public static class BotConfig
+    {
+        public static string Token = "Njk2Nzg4NzMxNTM2ODY3NDI4.Xp2fOg.FdpUSDm3G0OVRdsqWbwph2pJ2Cs";
+        public static string Prefix = "$";
+    }
+}
