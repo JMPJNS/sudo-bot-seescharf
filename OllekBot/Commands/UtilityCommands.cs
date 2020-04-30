0d73ddebd88623a80f4588910256f6e29abd5402
Jonas <jonas@jmp.blue>
2020-04-30 16:16:44 +0200

    added reminder command

--- OllekBot/Commands/UtilityCommands.cs
+++ OllekBot/Commands/UtilityCommands.cs
@@ -1,5 +1,6 @@
 using System;
-using System.Linq;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -28,6 +29,35 @@ namespace OllekBot.Commands
                 {
                     var message = await ctx.Channel.SendMessageAsync($"{await currentGuild.GetMemberAsync(user.UserId)}");
                 }
+            }
+        }
+
+        [Command("reminder")]
+        public async Task Reminder(CommandContext ctx, string format, DateTime time, string message)
+        {
+            await this.ReminderCommand(ctx, format, time, message);
+        }
+        
+        [Command("reminder")]
+        public async Task Reminder(CommandContext ctx, string format, TimeSpan time, string message)
+        {
+            await this.ReminderCommand(ctx, format, DateTime.Now + time, message);
+        }
+
+        private async Task ReminderCommand(CommandContext ctx, string format, DateTime time, string message)
+        {
+            var ts = time - DateTime.Now;
+            var thenTime = DateTime.Now;
+            
+            if (format == "um" || format == "in")
+            {
+                await ctx.Channel.SendMessageAsync($"Ich werde dich um {time.ToString("h:mm:ss", DateTimeFormatInfo.InvariantInfo)} erinnern");
+                await Task.Delay(ts);
+                await ctx.Channel.SendMessageAsync($"{ctx.User.Mention} [{thenTime.ToString("h:mm:ss", DateTimeFormatInfo.InvariantInfo)}], {message}");
+            } else
+            {
+                await ctx.Channel.SendMessageAsync("Falsch Verwendet! $reminder {um/in} {uhrzeit/zeitspanne} {nachricht}");
+                return;
             }
         }
     }
