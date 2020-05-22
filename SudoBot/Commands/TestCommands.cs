71df886ff3ba6fb73068463d37469f6ab172562e
Jonas <jonas@jmp.blue>
2020-05-22 22:01:23 +0200

    embed

--- SudoBot/Commands/TestCommands.cs
+++ SudoBot/Commands/TestCommands.cs
@@ -9,10 +9,12 @@ namespace SudoBot.Commands
     [Group("test")]
     public class TestCommands : BaseCommandModule
     {
-        [CheckForPermissions(SudoPermission.Any, GuildPermission.TestCommands)]
+        [CheckForPermissions(SudoPermission.Admin, GuildPermission.TestCommands)]
         [Command("test")]
-        public async Task T(CommandContext ctx, DiscordChannel channel, DiscordRole role)
+        public async Task T(CommandContext ctx, DiscordChannel channel)
         {
+
+
             var embed = new DiscordEmbedBuilder()
                 .WithTitle("Das IQ-System")
                 .WithColor(DiscordColor.Aquamarine)
@@ -23,19 +25,7 @@ Dieses IQ-Punkte stellen ein komplett neues Levelsystem da, mit dem ihr ab einer
  
 Spammen wird nicht belohnt und verstößt zudem noch gegen die {channel.Mention}, also haltet euch an diese!
 
-Außerdem könnt ihr auch durch Teilnahme an Contests etc. IQ verdienen, es lohnt sich also, auch an diesen teilzunehmen. ")
-                .AddField("*IQ*", "*Rang*", true)
-                .AddField("140", "Buschcamper")
-                .AddField("500", "Schwitzer")
-                .AddField("1.000", "Aluhut")
-                .AddField("2.500", "Bronze")
-                .AddField("5.000", "Silber")
-                .AddField("7.500", "Gold")
-                .AddField("10.000", role.Mention)
-                .AddField("15.000", "Diamant")
-                .AddField("20.000", "Master")
-                .AddField("25.000", "Legend")
-                .AddField("30.000", "Immortal");
+Außerdem könnt ihr auch durch Teilnahme an Contests etc. IQ verdienen, es lohnt sich also, auch an diesen teilzunehmen. ");
             await ctx.Channel.SendMessageAsync(embed: embed.Build());
 
         }
