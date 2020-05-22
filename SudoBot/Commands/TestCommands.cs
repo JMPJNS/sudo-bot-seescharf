990dfee3d31dfc9895e990c0e92c296d147a09a5
Jonas <jonas@jmp.blue>
2020-05-23 00:17:02 +0200

    remove cursed command again

--- SudoBot/Commands/TestCommands.cs
+++ SudoBot/Commands/TestCommands.cs
@@ -11,58 +11,6 @@ namespace SudoBot.Commands
     [Group("test")]
     public class TestCommands : BaseCommandModule
     {
-        [CheckForPermissions(SudoPermission.Me, GuildPermission.Any)]
-        [Command("test")]
-        public async Task Test(CommandContext ctx)
-        {
-            var users = await Mongo.Instance.GetAllUsersWithPrevRole();
-            foreach (var user in users)
-            {
-                int addMessages = 0;
-                if (user.HighestOldLevel == 1)
-                {
-                    addMessages = 1;
-                } else if (user.HighestOldLevel == 5)
-                {
-                    addMessages = 50;
-                } else if (user.HighestOldLevel == 10)
-                {
-                    addMessages = 150;
-                } else if (user.HighestOldLevel == 15)
-                {
-                    addMessages = 500;
-                } else if (user.HighestOldLevel == 20)
-                {
-                    addMessages = 700;
-                } else if (user.HighestOldLevel == 30)
-                {
-                    addMessages = 1000;
-                } else if (user.HighestOldLevel == 40)
-                {
-                    addMessages = 1200;
-                } else if (user.HighestOldLevel == 50)
-                {
-                    addMessages = 1400;
-                } else if (user.HighestOldLevel == 60)
-                {
-                    addMessages = 1600;
-                } else if (user.HighestOldLevel == 70)
-                {
-                    addMessages = 3000;
-                } else if (user.HighestOldLevel == 80)
-                {
-                    addMessages = 5000;
-                }
-                else
-                {
-                    addMessages = 0;
-                }
-
-                await user.AddCountedMessagesByHand(addMessages);
-                Console.WriteLine("Added");
-            }
-        }
-        
         
         [CheckForPermissions(SudoPermission.Admin, GuildPermission.TestCommands)]
         [Command("e")]
